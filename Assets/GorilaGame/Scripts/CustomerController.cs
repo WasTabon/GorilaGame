b90e60bc81d3e6773ba7e8edab43d00b1be82895
WasTabon <titanium.jen@gmail.com>
2025-09-23 22:57:31 +0300

    ui

--- Assets/GorilaGame/Scripts/CustomerController.cs
+++ Assets/GorilaGame/Scripts/CustomerController.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using TMPro;
 using UnityEngine;
 
@@ -5,16 +6,25 @@ public class CustomerController : MonoBehaviour
 {
     [SerializeField] private RectTransform _dialogueBox;
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField] private CookedFoodData _cookedFoodData;
     [SerializeField] private float _moveSpeed = 2f;
     [SerializeField] private float _stopDistance = 0.5f;
 
     private Animator _animator;
+
+    private CookedFoodType _requestedFood;
     
     private Transform _movePos1;
     private Transform _movePos2;
 
     private bool _isInitialized;
     private bool _reachedDestination;
+
+    private void Start()
+    {
+        _dialogueBox.DOScale(Vector3.zero, 0f);
+    }
+
     public void Initiallize(Transform movePos1, Transform movePo2)
     {
         _animator = GetComponent<Animator>();
@@ -24,7 +34,6 @@ public class CustomerController : MonoBehaviour
         _isInitialized = true;
         _reachedDestination = false;
         
-        // Начинаем движение
         SetWalkAnimation(true);
     }
 
@@ -42,12 +51,10 @@ public class CustomerController : MonoBehaviour
         
         if (distanceToTarget > _stopDistance)
         {
-            // Двигаемся к цели
             Vector3 direction = (_movePos2.position - transform.position).normalized;
             direction.y = 0f;
             transform.position += direction * _moveSpeed * Time.deltaTime;
             
-            // Поворачиваем персонажа в сторону движения
             if (direction != Vector3.zero)
             {
                 transform.rotation = Quaternion.LookRotation(direction);
@@ -55,10 +62,24 @@ public class CustomerController : MonoBehaviour
         }
         else
         {
-            // Достигли цели
             _reachedDestination = true;
             SetWalkAnimation(false);
-            Debug.Log("Customer reached destination!");
+
+            // Выбираем случайную еду
+            _requestedFood = (CookedFoodType)Random.Range(0, System.Enum.GetValues(typeof(CookedFoodType)).Length);
+            
+            // Устанавливаем название еды в текст
+            if (_cookedFoodData != null)
+            {
+                _text.text = "I want " + _cookedFoodData.GetCookedFoodName(_requestedFood);
+            }
+            else
+            {
+                _text.text = "I want " + _requestedFood.ToString();
+            }
+            
+            _dialogueBox.DOScale(Vector3.one, 0.5f)
+                .SetEase(Ease.InOutBack);
         }
     }
     
@@ -68,10 +89,11 @@ public class CustomerController : MonoBehaviour
         {
             _animator.SetBool("isWalk", isWalking);
             _animator.SetBool("isIdle", !isWalking);
-            Debug.Log("Walk");
-            Debug.Log(_animator.GetBool("IsWalk"));
-            Debug.Log("Idle");
-            Debug.Log(_animator.GetBool("isIdle"));
         }
+    }
+
+    public CookedFoodType GetRequestedFood()
+    {
+        return _requestedFood;
     }
 }
