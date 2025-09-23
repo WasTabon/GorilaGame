0eea8d1c0e9c3bc6ad17410b19262f80ed4ab47b
WasTabon <titanium.jen@gmail.com>
2025-09-23 21:11:11 +0300

    fix

--- Assets/GorilaGame/Scripts/CustomerController.cs
+++ Assets/GorilaGame/Scripts/CustomerController.cs
@@ -15,14 +15,10 @@ public class CustomerController : MonoBehaviour
 
     private bool _isInitialized;
     private bool _reachedDestination;
-
-    private void Start()
-    {
-        _animator = GetComponent<Animator>();
-    }
-
     public void Initiallize(Transform movePos1, Transform movePo2)
     {
+        _animator = GetComponent<Animator>();
+        
         _movePos1 = movePos1;
         _movePos2 = movePo2;
         _isInitialized = true;
@@ -48,7 +44,7 @@ public class CustomerController : MonoBehaviour
         {
             // Двигаемся к цели
             Vector3 direction = (_movePos2.position - transform.position).normalized;
-            direction.y = transform.position.y;
+            direction.y = 0f;
             transform.position += direction * _moveSpeed * Time.deltaTime;
             
             // Поворачиваем персонажа в сторону движения
@@ -72,6 +68,10 @@ public class CustomerController : MonoBehaviour
         {
             _animator.SetBool("isWalk", isWalking);
             _animator.SetBool("isIdle", !isWalking);
+            Debug.Log("Walk");
+            Debug.Log(_animator.GetBool("IsWalk"));
+            Debug.Log("Idle");
+            Debug.Log(_animator.GetBool("isIdle"));
         }
     }
 }
